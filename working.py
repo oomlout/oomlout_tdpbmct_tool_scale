0eba105b525b9498727472b1c863e419e890e49c
Oom Lout <aaron@oomlout.com>
2024-03-14 15:47:49 +0000

    working

--- working.py
+++ working.py
@@ -1,8 +1,7 @@
-import oom_kicad
-import oom_markdown
 import os
 import copy
-import scad
+import yaml
+import time
 
 #process
 #  locations set in working_parts.ods 
@@ -10,111 +9,226 @@ import scad
 #  put components on the right side of the board
 #  run this script
 
+#import a library to plot points onto an image
+import cv2
+import numpy as np
+
+#scales
+configuration_scale_name = "tool_scale_76_mm_width_114_mm_height_20_mm_depth_1000_gram_capacity_aliexpress_jewelry_scale"
+
 def main(**kwargs):
-    #place_parts(**kwargs)
-    #make_readme(**kwargs)
-    scad.make_scad(**kwargs)
+    create_scale_configuration()
+
+    configuration_file = "configuration/oomlout_tdpbmct_tool_scale.yaml"
+    with open(configuration_file, 'r') as file:
+        print(f"reading from {configuration_file}")
+        scale_configurations = yaml.load(file, Loader=yaml.FullLoader)
+
+    scale_configuration = scale_configurations[0]
+    print(f"scale_configuration: {scale_configuration["name"]}")
+
+    create_configuration_overlay_image(scale_configuration)
+
+    #from image
+    #value = get_reading_from_file(scale_configuration)
+
+    while True:
+        #from camera
+        value = get_reading_from_camera(scale_configuration)
+        print(f"value: {value}")
+        time.sleep(1)
+
+
+
     
+def create_configuration_overlay_image(scale_configuration):
+    image_test = "test_images/test_raw.jpg"
+
+    #import a library to plot points onto an image
+    import cv2
+    import numpy as np
+
+    #read the image
+    img = cv2.imread(image_test)
+
+    #plot the segment test coordinates one by one on the image
+    colors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255),(255,0,255),(255,255,255)]
+
+    number_of_digits = scale_configuration["number_of_digits"]
+    digit_start_coordinates = scale_configuration["digit_start_coordinates"]
+    segment_test_coordinates = scale_configuration["segment_test_coordinates"]
+    for i in range(number_of_digits):
+    #for i in range(1):
+        x = digit_start_coordinates[i][0]
+        y = digit_start_coordinates[i][1]
+        color = colors[i]
+        #add circle of color color
+        cv2.circle(img, (x,y), 10, color, -1)
+        #add image offsets
+        for j in range(len(segment_test_coordinates)):
+            xx = x + segment_test_coordinates[j][0]
+            yy = y + segment_test_coordinates[j][1]
+            cv2.circle(img, (xx,yy), 5, color, -1)
+
+    #save the image
+    image_overlay = "test_images/test_overlay.jpg"
+    print(f"saving to {image_overlay}")
+    cv2.imwrite(image_overlay, img)
+
+
+
+
     
 
-def make_readme(**kwargs):
-    os.system("generate_resolution.bat")
-    oom_markdown.generate_readme_project(**kwargs)
-    #oom_markdown.generate_readme_teardown(**kwargs)
-    
-def make_scad(**kwargs):
-    import opsc
-    import oobb 
-    import oobb_base
 
-    kwargs["save_type"] = "none"
-    #kwargs["save_type"] = "all"
-    kwargs["size"] = "oobb"
-    kwargs["type"] = "oomlout_bolt_tool_funnel"
-    kwargs["width"] = 3
-    kwargs["height"] = 5
-    kwargs["thickness"] = 6
-     # default sets
-    width = kwargs.get("width", 3)
-    height = kwargs.get("height", 5)
-    thickness = kwargs.get("thickness", 3)
-    size = kwargs.get("size", "oobb")
-    pos = kwargs.get("pos", [0, 0, 0])
-    # extra sets
-    holes = kwargs.get("holes", True)
-    both_holes = kwargs.get("both_holes", True)    
-    kwargs["pos"] = pos
-    
-        # get the default thing
-    thing = oobb_base.get_default_thing(**kwargs)
-    th = thing["components"]
-    kwargs.pop("size","")
 
-    th.append(oobb_base.get_comment("plate main","p"))
-    # add plate
-    p3 = copy.deepcopy(kwargs)
-    p3["type"] = "p"   
-    p3["shape"] = f"{size}_plate"
-    p3["width"] = width
-    p3["height"] = height  
-    p3["depth"] = thickness
-    p3["pos"] = pos
-    #p3["m"] = ""  
-    oobb_base.append_full(thing,**p3)      
-    #th.append(oobb_base.oobb_easy(**p3))
+
+def create_scale_configuration():
+    file_configuration  = "configuration/oomlout_tdpbmct_tool_scale.yaml"
+
+    scale_configurations = []
+
+    scale_name = "tool_scale_76_mm_width_114_mm_height_20_mm_depth_1000_gram_capacity_aliexpress_jewelry_scale"
+    scale_configuration = {}
+    scale_configuration["name"] = scale_name  
+    scale_configuration["display_style"] = "seven_segment"
+    scale_configuration["number_of_digits"] = 5
+    #configuration of digits
+    scale_configuration["digit_start_coordinates"] = []
+    x = 606
+    y = 130
+    scale_configuration["digit_start_coordinates"].append([x,y])
+    shift_x = -100
+    shift_y = 0
+    for i in range(6):
+        x += shift_x
+        y += shift_y
+        scale_configuration["digit_start_coordinates"].append([x,y])
+        
+
+
+    #configuration of segments
+    #clockwise from top middle is last
+    scale_configuration["segment_test_coordinates"] = []
+    x_left_top = 18
+    x_left_bottom = 10
+    x_middle = 43
+    x_right_top = 72
+    x_right_bottom = 64
     
-    # add holes
-    if holes:
-        th.append(oobb_base.get_comment("holes main","n"))
-        p3 = copy.deepcopy(kwargs)
-        p3["type"] = "n"
-        p3["shape"] = f"{size}_holes"
-        p3["width"] = width
-        p3["height"] = height
-        p3["pos"] = pos
-        p3["both_holes"] = both_holes
-        #p3["m"] = ""
-        oobb_base.append_full(thing,**p3)      
-        #th.extend(oobb_base.oobb_easy(**p3))   
+    y_top = 14
+    y_top_middle = 44
+    y_middle = 83
+    y_bottom_middle = 115
+    y_bottom = 149
+    
+    scale_configuration["segment_test_coordinates"].append([x_middle,y_top])
+    scale_configuration["segment_test_coordinates"].append([x_right_top,y_top_middle])
+    scale_configuration["segment_test_coordinates"].append([x_right_bottom,y_bottom_middle])
+    scale_configuration["segment_test_coordinates"].append([x_middle,y_bottom])
+    scale_configuration["segment_test_coordinates"].append([x_left_bottom,y_bottom_middle])
+    scale_configuration["segment_test_coordinates"].append([x_left_top,y_top_middle])
+    scale_configuration["segment_test_coordinates"].append([x_middle,y_middle])
+
+    scale_configurations.append(scale_configuration)
+
+    #save configuration
+    with open(file_configuration, 'w') as file:
+        print(f"writing to {file_configuration}")
+        documents = yaml.dump(scale_configurations, file)
+
+
+def get_reading_from_file(scale_configuration):
+    image_name = "test_images/test.jpg"
+    #read the image
+    img = cv2.imread(image_name)
+
+    return get_reading_from_image(img, scale_configuration)
+
+def get_reading_from_camera(scale_configuration):
+    #create a video capture object
+    cap = cv2.VideoCapture(1)
+    #set 720 resolution
+    cap.set(3, 1280)
+    #get an image from the device    
+    ret, frame = cap.read()
+    #release the video capture object
+    cap.release()
+
+    image = frame
+
+    #save image as test_images/test_raw.jpg
+    image_name = "test_images/test_raw.jpg"
+    print(f"saving to {image_name}")
+    cv2.imwrite(image_name, image)
+    
+
+    return get_reading_from_image(image, scale_configuration)
+
+def get_reading_from_image(image, scale_configuration):
+    #read the image
+    
+    segments = []
+    for i in range(scale_configuration["number_of_digits"]):
+        segment = []
+        digit_start_coordinates = scale_configuration["digit_start_coordinates"][i]
+        for j in range(7):
+            segment_x = digit_start_coordinates[0] + scale_configuration["segment_test_coordinates"][j][0]
+            segment_y = digit_start_coordinates[1] + scale_configuration["segment_test_coordinates"][j][1]
+            #get the brigtness for a 3x3 pixel area
+            brightness_total = 0
+            for x in range(-1,2):
+                for y in range(-1,2):
+                    pixel_value = image[segment_y+y, segment_x+x]
+                    #reds int value
+                    red_int = int(pixel_value[2])
+                    green_int = int(pixel_value[1])
+                    blue_int = int(pixel_value[0])
+                    #brightness_pixel = (red_int + green_int + blue_int) / 3
+                    brightness_pixel = green_int
+                    brightness_total += brightness_pixel/9
+            threshold = 240
+            if brightness_total > threshold:
+                segment.append(1)
+            else:
+                segment.append(0)        
+        segment_original = copy.deepcopy(segment)
+        segment = 0
+        #segment.append(brightness_total)
+        segment_test = []
+        segment_test.append([0,0,0,0,0,0,1]) #0
+        segment_test.append([1,0,0,1,1,1,1]) #1
+        segment_test.append([0,0,1,0,1,0,0]) #2
+        segment_test.append([0,0,0,0,1,1,0]) #3
+        segment_test.append([1,0,0,1,1,0,0]) #4
+        segment_test.append([0,1,0,0,1,0,0]) #5
+        segment_test.append([0,1,0,0,0,0,0]) #6
+        segment_test.append([0,0,0,0,1,1,1]) #7
+        segment_test.append([0,0,0,0,0,0,0]) #8
+        segment_test.append([0,0,0,0,1,0,0]) #9
         
         
-        save_type = kwargs.get("save_type", "all")
-        overwrite = True
-        modes = ["3dpr", "laser", "true"]
-        for mode in modes:
-            depth = thing.get(
-                "depth_mm", thing.get("thickness_mm", 3))
-            height = thing.get("height_mm", 100)
-            layers = depth / 3
-            tilediff = height + 10
-            start = 1.5
-            if layers != 1:
-                start = 1.5 - (layers / 2)*3
-            if "bunting" in thing:
-                start = 0.5
-            opsc.opsc_make_object(f'scad_output/{thing["id"]}/{mode}.scad', thing["components"], mode=mode, save_type=save_type, overwrite=overwrite, layers=layers, tilediff=tilediff, start=start)
+        
+        for i in range(0,len(segment_test)):
+            match = True
+            for j in range(0,len(segment_test[i])):
+                if segment_test[i][j] != segment_original[j]:
+                    match = False
+                    break            
+            if match:
+                segment = i
             
- 
-
-
-#take component positions from working_parts.csv and place them in working.kicad_pcb
-def place_parts(**kwargs):
-    board_file = "kicad/current_version/working/working.kicad_pcb"
-    parts_file = "working_parts.csv"
-    #load csv file
-    import csv
-    with open(parts_file, 'r') as f:
-        reader = csv.DictReader(f)
-        parts = [row for row in reader]
-
-
-    
-    oom_kicad.kicad_set_components(board_file=board_file, parts=parts, corel_pos=True, **kwargs)
-
-
-
-
-
+            
+            
+        segments.append(segment)
+    reading = 0
+    for i in range(len(segments)-1,-1,-1):
+        reading = reading * 10 + segments[i]
+        
+    reading = reading/10
+    return reading
 
 if __name__ == '__main__':
-    main()
+    kwargs = {}
+
+    main(**kwargs)
